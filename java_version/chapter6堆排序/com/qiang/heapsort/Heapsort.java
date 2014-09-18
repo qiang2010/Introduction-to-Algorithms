b6c7fbcd396c89a398ab92b3013bce7b62e8c205
qiang2010 <qiangji1991@gmail.com>
2014-09-18 20:46:29 +0800

    heapsort

---
+++ java_version/chapter6堆排序/com/qiang/heapsort/Heapsort.java
@@ -0,0 +1,32 @@
+package com.qiang.heapsort;
+
+/*
+ *  该类主要是实现堆排序
+ *  数据存放在一个一位数组中。
+ *  code by qiangji 
+ */
+
+
+public class Heapsort<T> {
+
+	private int maxsize;
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	// 制定size大小
+	public Heapsort(int size){
+		maxsize = size;
+		
+	}
+	public Heapsort(){
+		maxsize = 100;
+	}
+	
+	
+
+}
